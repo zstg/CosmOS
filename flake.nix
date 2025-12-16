425597e5ea7a5de0aba95ae3437a2f2b3041ab9b
zstg <o0vckutt@duck.com>
2025-12-16 18:16:32 +0530

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -2,198 +2,190 @@
   description = "Stig's NixOS ISOs";
 
   inputs = {
-    self.submodules = true; # since this flake uses submodules. This is purely optional, but it means one does not have to specify `?submodules=1` when checking this flake or building the ISO.
+    self.submodules = true;
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     sddm-astronaut-theme = {
       url = "github:stratos-linux/sddm-astronaut-theme";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     vicinae.url = "github:vicinaehq/vicinae";
+
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     stratos-wallpapers = {
       url = "github:stratos-linux/stratos-wallpapers";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, disko, stylix, vicinae, zen-browser, sddm-astronaut-theme, ... } @ inputs:
+  outputs = inputs@{ self, nixpkgs, home-manager, disko, stylix, vicinae, zen-browser, sddm-astronaut-theme, ... }:
     let
-      system = "x86_64-linux";
+      system  = "x86_64-linux";
       version = "26.05";
-      
-      # Common ISO base
-      commonIsoBase = edition: modules: nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs system; };
-        modules = [
-          disko.nixosModules.disko 
-          {
-            disko.devices = {
-              disk = {
-                main = {
-                  device = "/dev/disk/by-id/some-id"; # When using disko-install, we will overwrite this value from the commandline
-                  type = "disk";
-                  content = {
-                    type = "gpt";
-                    partitions = {
-                      ESP = {
-                        type = "EF00";
-                        size = "1G";
-                        priority = 1; # set 1 esp on
-                        content = {
-                          type = "filesystem";
-                          format = "vfat";
-                          mountpoint = "/boot";
-                          mountOptions = [ "umask=0777" ];
-                        };
-                      };
-                      root = {
-                        size = "100%";
-                        content = {
-                          type = "filesystem";
-                          format = "ext4";
-                          mountpoint = "/";
-                        };
-                      };
-                    };
-                  };
+      lib     = nixpkgs.lib;
+
+      baseDiskoModule = {
+        disko.devices.disk.main = {
+          device = "/dev/disk/by-id/some-id"; # overridden by disko-install
+          type   = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                type     = "EF00";
+                size     = "1G";
+                priority = 1;
+                content = {
+                  type        = "filesystem";
+                  format      = "vfat";
+                  mountpoint  = "/boot";
+                  mountOptions = [ "umask=0777" ];
                 };
-                # other_disk = {};
+              };
+
+              root = {
+                size = "100%";
+                content = {
+                  type       = "filesystem";
+                  format     = "ext4";
+                  mountpoint = "/";
+                };
               };
             };
-          }
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
-          ./hosts/common
-          ({ pkgs, lib, ... }: {
-            boot.supportedFilesystems.zfs = lib.mkForce false;
-            environment.systemPackages = [ zen-browser.packages.${system}.default ];
-
-            isoImage = {
-              edition = edition;
-              isoName = "CosmOS${if edition != "" then "-${edition}" else ""}-#{self.lastModified}";
-              makeEfiBootable = true;
-              makeUsbBootable = true;
-            };
-          })
-        ] ++ modules;
-      };
-
-      # Common Home Manager config - FIXED: No system arg needed
-      mkHomeManager = { user, stateVersion }: home-manager.nixosModules.home-manager {
-        home-manager = {
-          backupFileExtension = "/tmp/${toString self.lastModified}.bak";
-          sharedModules = [
-            vicinae.homeManagerModules.default
-          ];
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.${user} = {
-            inherit stateVersion;
           };
         };
       };
 
-      # Common standalone home configs
-      mkStandaloneHome = name: hostDir: home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          ./hosts/${hostDir}/home.nix
-          stylix.homeManagerModules.stylix
-        ];
-        extraSpecialArgs = { inherit inputs system; };
+      # spin descriptor drives both nixosConfiguration and ISO derivation
+      spins = {
+        hyprland = {
+          hostDir       = "CosmOS-Hyprland";
+          edition       = "hyprland";
+          withStylix    = true;
+          withSddmTheme = true;
+          withVicinae   = true;
+        };
+
+        niri = {
+          hostDir       = "CosmOS-Niri";
+          edition       = "niri";
+          withStylix    = true;
+          withSddmTheme = true;
+          withVicinae   = true;
+        };
+
+        gnome = {
+          hostDir       = "CosmOS-GNOME";
+          edition       = "gnome";
+          withStylix    = true;
+          withSddmTheme = false;
+          withVicinae   = false;  # GNOME: no vicinae
+        };
       };
+
+      mkNixosForSpin = spin:
+        let
+          inherit (spin) hostDir edition withStylix withSddmTheme withVicinae;
+        in
+        lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs system; };
+
+          modules =
+            [
+              disko.nixosModules.disko
+              baseDiskoModule
+              "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
+              ./hosts/${hostDir}
+              {
+                boot.supportedFilesystems.zfs = lib.mkForce false;
+
+                environment.systemPackages = [
+                  inputs.zen-browser.packages.${system}.default
+                ];
+
+                isoImage = {
+                  inherit edition;
+                  isoName         = "CosmOS${if edition != "" then "-${edition}" else ""}-${toString self.lastModified}";
+                  makeEfiBootable = true;
+                  makeUsbBootable = true;
+                };
+
+                imports = [ home-manager.nixosModules.home-manager ];
+
+                home-manager = {
+                  # useGlobalPkgs   = true;
+                  useUserPackages = true;
+
+                  sharedModules =
+                    [ ]
+                    ++ lib.optional withVicinae  vicinae.homeManagerModules.default;
+
+                  users.nixos = {
+                    imports = [ ./hosts/${hostDir}/home.nix ];
+                    home.stateVersion = version;
+                  };
+                };
+              }
+            ]
+            ++ lib.optional withStylix stylix.nixosModules.stylix
+            ++ lib.optional withSddmTheme sddm-astronaut-theme.nixosModules.default;
+        };
+
+      mkStandaloneHome = name: hostDir:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./hosts/${hostDir}/home.nix
+          ];
+          extraSpecialArgs = { inherit inputs system; };
+        };
+
+      # concrete nixosConfigurations
+      nixosCfgs = {
+        Hyprland = mkNixosForSpin spins.hyprland;
+        Niri     = mkNixosForSpin spins.niri;
+        Gnome    = mkNixosForSpin spins.gnome;
+      };
+
     in {
+      nixosConfigurations = nixosCfgs;
+
+      # ISO build packages (for `nix build .#Hyprland`, etc.)
       packages.${system} = {
-        hyprland-iso = self.nixosConfigurations.hyprland-iso.config.system.build.isoImage;
-        niri-iso = self.nixosConfigurations.niri-iso.config.system.build.isoImage;
-        # gnome-iso = self.nixosConfigurations.gnome-iso.config.system.build.isoImage;
-        default = self.packages.${system}.hyprland-iso;
+        Hyprland = nixosCfgs.Hyprland.config.system.build.isoImage;
+        Niri     = nixosCfgs.Niri.config.system.build.isoImage;
+        Gnome    = nixosCfgs.Gnome.config.system.build.isoImage;
+        default  = self.packages.${system}.Hyprland;
       };
 
-      nixosConfigurations = {
-        # TODO create a minimal ISO
-        hyprland-iso = commonIsoBase "hyprland" [
-          ./hosts/CosmOS-Hyprland
-          stylix.nixosModules.stylix
-
-          # This configures and sets up SDDM with the StratOS astronaut theme. No need to enable SDDM anywhere in the config.
-          sddm-astronaut-theme.nixosModules.default # TODO migrate this to WM-spins...?
-          # include the module *directly*
-          home-manager.nixosModules.home-manager
-
-          # then configure Home Manager options
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.sharedModules = [
-              vicinae.homeManagerModules.default
-            ];
-            home-manager.users.nixos = {
-              imports = [ ./hosts/CosmOS-Hyprland/home.nix ];
-              home.stateVersion = version;
-            };
-          }
-        ];
-
-        niri-iso = commonIsoBase "niri" [
-          ./hosts/CosmOS-Niri
-          stylix.nixosModules.stylix
-
-          # This configures and sets up SDDM with the StratOS astronaut theme. No need to enable SDDM anywhere in the config.
-          sddm-astronaut-theme.nixosModules.default # TODO migrate this to WM-spins...?
-          # include the module *directly*
-          home-manager.nixosModules.home-manager
-
-          # then configure Home Manager options
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.sharedModules = [
-              vicinae.homeManagerModules.default
-            ];
-            home-manager.users.nixos = {
-              imports = [ ./hosts/CosmOS-Niri/home.nix ];
-              home.stateVersion = version;
-            };
-          }
-        ];
-
-        gnome-iso = commonIsoBase "gnome" [
-          ./hosts/CosmOS-GNOME
-          stylix.nixosModules.stylix
-
-          home-manager.nixosModules.home-manager
-
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nixos = {
-              imports = [ ./hosts/CosmOS-GNOME/home.nix ];
-              home.stateVersion = version;
-            };
-          }
-        ];
-      };
-
+      # Standalone HM configs (non-ISO use)
       homeConfigurations = {
-        hyprland-iso = mkStandaloneHome "hyprland" "hyprland-iso";
-        niri-iso = mkStandaloneHome "niri" "niri-iso";
-        # gnome-iso = mkStandaloneHome "gnome" "gnome-iso";
+        Hyprland = mkStandaloneHome "hyprland" "CosmOS-Hyprland";
+        Niri     = mkStandaloneHome "niri"     "CosmOS-Niri";
+        Gnome = mkStandaloneHome "gnome"       "CosmOS-GNOME";
       };
     };
 }
